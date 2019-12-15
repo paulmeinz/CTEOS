9455382c19e3567d223fae2ab8a287d448d29c28
paulmeinz <paulmeinz@gmail.com>
2019-12-14 21:14:40 -0800

    CHANGED DESCRIPTION TO INCLUDE AUTHOR (PAUL MEINZ)

--- R/loadCTEOS.R
+++ R/loadCTEOS.R
@@ -11,14 +11,26 @@ loadCTEOS <- function(filepath, recode = TRUE) {
   data <- read.csv(filepath, stringsAsFactors = F,
                    na.strings = '#NULL!')
   codes <- unique(codebook$index)
-  for (i in codes) {
-    print(i)
+
+  recodes <- codes[!grepl('Fewer',codes)]
+
+  for (i in recodes) {
     code <- codebook[codebook$index == i,]
     lookup <- code$Label
     names(lookup) <- code$value
     if (i %in% names(data)) {
       names(data)[names(data) == i] <- 'rec'
       data[,'rec'] <- lookup[data[,'rec']]
+      names(data)[names(data) == 'rec'] <- i
+    }
+  }
+
+  nonrecodes <- codes[grepl('Fewer',codes)]
+
+  for (i in nonrecodes) {
+    if (i %in% names(data)) {
+      names(data)[names(data) == i] <- 'rec'
+      data$rec[is.na(data$rec)] <- 0
       names(data)[names(data) == 'rec'] <- i
     }
   }
