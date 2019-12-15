3abd0a0da4f96e1d0165ba32d707b0e24ce4f867
paulmeinz <paulmeinz@gmail.com>
2019-12-14 20:35:31 -0800

    ADDED RECODE LOOP TO loadCTEOS

--- R/loadCTEOS.R
+++ R/loadCTEOS.R
@@ -1,18 +1,26 @@
-#' Add together two numbers.
+#' Load and Clean CTEOS Data
 #'
 #' @param filepath Path to your CTEOS file.
 #' @param recode default as TRUE, if you want to recode variables from the
 #' codebook.
-#' @return A loaded CTEOS dataset
-#' @examples
-#' \dontrun {
-#' loadCTEOS('cteosfile.csv', recode = TRUE)
-#' }
+#' @return A CTEOS dataset as a dataset
 #' @export
 
 
 loadCTEOS <- function(filepath, recode = TRUE) {
   data <- read.csv(filepath, stringsAsFactors = F,
                    na.strings = '#NULL!')
-
+  codes <- unique(codebook$index)
+  for (i in codes) {
+    print(i)
+    code <- codebook[codebook$index == i,]
+    lookup <- code$Label
+    names(lookup) <- code$value
+    if (i %in% names(data)) {
+      names(data)[names(data) == i] <- 'rec'
+      data[,'rec'] <- lookup[data[,'rec']]
+      names(data)[names(data) == 'rec'] <- i
+    }
+  }
+  data
 }
